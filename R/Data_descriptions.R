f78ad1b728203ad22fb8cf18de6b950c14d2d043
Ardern Hulme-Beaman <ardernhb@gmail.com>
2021-01-08 17:09:28 +0000

    upload of black rat test data.

--- R/Data_descriptions.R
+++ R/Data_descriptions.R
@@ -3,8 +3,8 @@
 #'
 #' A geometric morphometric derived dataset from the data of Hulme-Beaman et al.
 #' 2019. The dataset contains an array of 395 specimens each with 105 landmarks.
-#' Scale data is also included as is individual specimen info. A matrix of full
-#' Procrustes distances calculated among the specimens is also provided.
+#' Centroid size data is also included as is individual specimen info. A matrix
+#' of full Procrustes distances calculated among the specimens is also provided.
 #'
 #' @format A list of 2 objects:
 #' \describe{
@@ -13,6 +13,21 @@
 #' }
 #' @source \url{https://doi.org/10.1007/s10914-017-9423-8}
 "RatData"
+
+
+
+#' Black Rat shape dataset
+#'
+#' A geometric morphometric derived dataset containing an data from 100 specimens
+#' with 120 landmarks all collected from Mainland Southeast Asia.
+#'
+#' @format A list of 2 objects:
+#' \describe{
+#'   \item{LMArray}{Specimen landmark data in array form, where each row is a landmark, each column a landmark dimension and each slice is an individual specimen. These landmarks have been scaled.}
+#' }
+#' @source \url{https://doi.org/10.1007/s10914-017-9423-8}
+"BlackRat"
+
 
 
 
